fef1bc0e66b4a25b590a793bae81a47b23f69db4
Sydney James <sydneyjames7@gmail.com>
2020-11-16 16:17:25 -0800

    made users, events, and recipes js

---
+++ data/events.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    title: 'Thanksgiving',
+    date: '2020-11-26',
+    members: 'http://placekitten.com/200/300',
+  },
+  {
+    title: 'Christmas',
+    date: '2020-12-25',
+    members: 'http://placekitten.com/200/300',
+  },
+  {
+    title: 'Valentines',
+    date: '2021-02-14',
+    members: 'http://placekitten.com/200/300',
+  }
+];
